9bae9e735ec2c66a2b495faac456a89029b154af
MehwishI <mehwishned21@gmail.com>
2023-11-26 12:51:44 -0600

    Add feature dashboard count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
   def show
+    #count number of products
+    @count_products = Product.count
+
+    #count number of categories
+    @count_categories= Category.count
   end
 end
