994ce8e47dd2d91c92f93ffca1c9c8b23d7d232a
MehwishI <mehwishned21@gmail.com>
2023-11-30 22:59:27 -0600

    add feature user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
 
   get '/about', to: 'about#index'
 
-
+    # These routes will be for signup. The first renders a form in the browse, the second will 
+    # receive the form and create a user in our database using the data given to us by the user.
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+    get '/login'  => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/logout'  => 'sessions#destroy'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
